7f8b685e4d884a91146c182e519487171ba0f347
Tom Riis <tomriis@Toms-MacBook-Air.local>
2018-08-29 02:06:42 -0600

    file loading time varying field for insert_electracellular()

--- TemporalInterference/e_potential_gen.py
+++ TemporalInterference/e_potential_gen.py
@@ -1,4 +1,13 @@
 # Module to generate time varying electric field and save as a series of txt files
+import numpy as np
 
-def uniform_field(X, Y, Z, N):
+def uniform_field(filename, V):
+    Y_lim=[-200,200]
+    field = np.zeros((len( range(Y_lim[0],Y_lim[1]))))
+    for i, y in enumerate(range(Y_lim[0],Y_lim[1])):
+        field[i]=V
+    np.savetxt(filename, field)
+        
+        
+    
     
